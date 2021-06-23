ef300dd477f6c2b55103357828755bc07962c692
zbirenbaum <zacharyobirenbaum@gmail.com>
2021-06-23 15:23:41 -0400

    results

--- summary.py
+++ summary.py
@@ -51,7 +51,7 @@ def run(logdir):
                 if rank <= 5 and rank >= 0:
                     r5 += 1
     
-                '''if rank > 0:
+                if rank > 0:
                     ta += 1
                     accScores.append(score)
     
@@ -65,7 +65,7 @@ def run(logdir):
     
                 if rank == -2:
                     fa += 1
-                    rejScores.append(score)'''
+                    rejScores.append(score)
     
             for k in range(len(curve)):
                 curve[k] /= count
@@ -79,48 +79,48 @@ def run(logdir):
             print(rank1)
             print(rank5)
             return rank1, rank5
-#meanr1 = sum(rank1)/len(rank1)
-#meanr5 = sum(rank5)/len(rank5)
-#
-#print('Rank 1')
-#print(' mean:  {0}'.format(meanr1))
-#print(' median: {0}'.format(np.median(np.array(rank1))))
-#print(' min:   {0}'.format(min(rank1)))
-#print(' max:   {0}'.format(max(rank1)))
-#print(' stdev: {0}'.format(np.std(np.array(rank1))))
-#print
-#if (fa+tr != 0):
-#    print(' TA:   {0}'.format(ta/float(ta+fr)))
-#    print(' FA:   {0}'.format(fa/float(fa+tr)))
-#    print('{0},{1}\n'.format(ta/float(ta+fr),fa/float(fa+tr)))
-#    plt.hist(np.array(rejScores),bins=5,normed=True,hold=True,color='r',alpha=0.5,label='No Match in Gallery')
-#    plt.hist(np.array(accScores),bins=25,normed=True,hold=True,color='g',alpha=0.5,label='Match in Gallery')
-#    plt.plot((-.83,-.83),(0,40),hold=True)
-#    plt.xlabel('Match Score')
-#    plt.ylabel('% of accept/reject images')
-#    plt.legend()
-#    plt.show()
-#
-#print('Rank 5')
-#print(' mean:  {0}'.format(meanr5))
-#print(' median: {0}'.format(np.median(np.array(rank5))))
-#print(' min:   {0}'.format(min(rank5)))
-#print(' max:   {0}'.format(max(rank5)))
-#
-#cmcs = np.array(ranksets) * 100
-#minCMC = cmcs.min(0)
-#maxCMC = cmcs.max(0)
-#meanCMC = cmcs.mean(0)
-#
-#plt.plot(np.array(range(1,6)),minCMC,'r-',hold=True,label='min')
-#plt.plot(np.array(range(1,6)),meanCMC,'b-',hold=True,label='mean')
-#plt.plot(np.array(range(1,6)),maxCMC,'g-',hold=True,label='max')
-#plt.axis([1,5,80,102])
-#plt.yticks([i for i in range(80,101,2)])
-#plt.xticks([1,2,3,4,5])
-#plt.grid()
-#plt.legend(loc='lower-right')
-#plt.title('Closed-Set')
-#plt.xlabel('Rank')
-#plt.ylabel('Cumulative Accuracy (%)')
-#plt.show()
+            meanr1 = sum(rank1)/len(rank1)
+            meanr5 = sum(rank5)/len(rank5)
+
+            print('Rank 1')
+            print(' mean:  {0}'.format(meanr1))
+            print(' median: {0}'.format(np.median(np.array(rank1))))
+            print(' min:   {0}'.format(min(rank1)))
+            print(' max:   {0}'.format(max(rank1)))
+            print(' stdev: {0}'.format(np.std(np.array(rank1))))
+            print
+            if (fa+tr != 0):
+                print(' TA:   {0}'.format(ta/float(ta+fr)))
+                print(' FA:   {0}'.format(fa/float(fa+tr)))
+                print('{0},{1}\n'.format(ta/float(ta+fr),fa/float(fa+tr)))
+                plt.hist(np.array(rejScores),bins=5,normed=True,hold=True,color='r',alpha=0.5,label='No Match in Gallery')
+                plt.hist(np.array(accScores),bins=25,normed=True,hold=True,color='g',alpha=0.5,label='Match in Gallery')
+                plt.plot((-.83,-.83),(0,40),hold=True)
+                plt.xlabel('Match Score')
+                plt.ylabel('% of accept/reject images')
+                plt.legend()
+                plt.show()
+
+            print('Rank 5')
+            print(' mean:  {0}'.format(meanr5))
+            print(' median: {0}'.format(np.median(np.array(rank5))))
+            print(' min:   {0}'.format(min(rank5)))
+            print(' max:   {0}'.format(max(rank5)))
+
+            cmcs = np.array(ranksets) * 100
+            minCMC = cmcs.min(0)
+            maxCMC = cmcs.max(0)
+            meanCMC = cmcs.mean(0)
+
+            plt.plot(np.array(range(1,6)),minCMC,'r-',hold=True,label='min')
+            plt.plot(np.array(range(1,6)),meanCMC,'b-',hold=True,label='mean')
+            plt.plot(np.array(range(1,6)),maxCMC,'g-',hold=True,label='max')
+            plt.axis([1,5,80,102])
+            plt.yticks([i for i in range(80,101,2)])
+            plt.xticks([1,2,3,4,5])
+            plt.grid()
+            plt.legend(loc='lower-right')
+            plt.title('Closed-Set')
+            plt.xlabel('Rank')
+            plt.ylabel('Cumulative Accuracy (%)')
+            plt.show()
