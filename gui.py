474668813fe4b7f5a924d1d86a026452dbbff279
Hieu Do <hdo@colgate.edu>
2021-08-02 17:02:27 -0400

    Finalize v.1.0.0 GUI

--- gui.py
+++ gui.py
@@ -120,15 +120,13 @@ class GUI(ttk.Frame):
         self.probelabel = list(self.data.keys()) # contains all probe images
         self.current = 0
         self.maxPhotos = 5
-        self.rank = 5
-        self.curProbeImgWidget = []
-        self.curRankPred = []         
+        self.rank = 5       
         
         self.init_gui()
 
     def init_gui(self):
         self.root.title('SealNet')
-        self.root.geometry("1200x650")
+        self.root.geometry("1100x900")
         self.grid(column=0, row=0, sticky='nsew')
         #self.grid_columnconfigure(0, weight=1) # Allows column to stretch upon resizing
         #self.grid_rowconfigure(0, weight=1) # Same with row
@@ -141,21 +139,46 @@ class GUI(ttk.Frame):
 
         # Button Bar
         self.buttonBar = ttk.Frame(self)
-        self.buttonBar.grid(row=0, column=0)
+        self.buttonBar.grid(row=0, column=0, pady=15)
 
-        self.buttonBarDict = {}
-        self.buttonBarDict[(0,0)] = ttk.Button(self.buttonBar, text="Previous", command=self.prev).grid(row=0, column=0)
-        self.buttonBarDict[(0,1)] = ttk.Label(self.buttonBar, text="").grid(row=0, column=1)
-        self.buttonBarDict[(0,2)] = ttk.Button(self.buttonBar, text="Next", command=self.next).grid(row=0, column=2)
+        self.buttonBarDict = {} # to easily access elements in buttonBar Fram
+        self.buttonBarDict[(0,0)] = ttk.Button(self.buttonBar, text="Previous", command=self.prev)
+        self.buttonBarDict[(0,1)] = ttk.Label(self.buttonBar, text="")
+        self.buttonBarDict[(0,2)] = ttk.Button(self.buttonBar, text="Next", command=self.next)
+        for x,y in self.buttonBarDict.keys():
+            self.buttonBarDict[(x,y)].grid(row=x, column=y, padx=30, pady=5)
 
         # Main Frame
         self.mainFrame = ttk.Frame(self)
-        self.mainFrame.grid(row=2, column=0)
-        
+        self.mainFrame.grid(row=1, column=0)
+
+        # Add Probe and Label title
+        ttk.Label(self.mainFrame, text="Probe", font=('Arial', 25)).grid(row=0, column=0)
+        ttk.Label(self.mainFrame, text="Label", font=('Arial', 25)).grid(row=0, column=1, columnspan=3+self.maxPhotos)
+        # Add Probe Frame
+        probeFrame = tkinter.Frame(self.mainFrame, highlightbackground="black", highlightthickness=1)
+        probeFrame.grid(row=1, column=0, padx=15, pady=15)
+        self.probeFrameDict = {}
+        for i in range(self.maxPhotos):
+            self.probeFrameDict[(i, 0)] = ttk.Label(probeFrame, image=None)
+            self.probeFrameDict[(i, 0)].grid(row=i, column=0, padx=10, pady=10)
+        # Add Gallery Frame
+        galleryFrame = tkinter.Frame(self.mainFrame, highlightbackground="black", highlightthickness=1)
+        galleryFrame.grid(row=1, column=1, pady=15, ipadx=15, ipady=15)
+        self.galleryFrameDict = {}
+        for i in range(self.rank):
+            self.galleryFrameDict[(2*i, 0)] = ttk.Label(galleryFrame, text='Rank {}'.format(i+1))
+            self.galleryFrameDict[(2*i, 0)].grid(row=2*i, column=0, rowspan=2, padx=(15,0))
+            self.galleryFrameDict[(2*i, 1)] = ttk.Label(galleryFrame, text='Score: ')
+            self.galleryFrameDict[(2*i, 1)].grid(row=2*i, column=1, rowspan=2, padx=15)
+            self.galleryFrameDict[(2*i, 2)] = ttk.Label(galleryFrame, text='')
+            self.galleryFrameDict[(2*i, 2)].grid(row=2*i, column=2, columnspan=self.maxPhotos)
+            for j in range(self.maxPhotos):
+                self.galleryFrameDict[(2*i+1,j+2)] = ttk.Label(galleryFrame, image=None)
+                self.galleryFrameDict[(2*i+1,j+2)].grid(row=2*i+1, column=j+2, padx=5, pady=5)
+                
         # Check if there is no probe
-        if (len(self.probelabel) == 0):
-            ttk.Label(self, text="There are no probe images to display")
-        else:
+        if (len(self.probelabel) > 0):
             self.loadCurrentImage()
         
     def getPhotosFromDir(self, dir):
@@ -173,30 +196,27 @@ class GUI(ttk.Frame):
     # Loading the main frame of the GUI displaying the current probe result
     def loadCurrentImage(self):
         curProbe = self.probelabel[self.current]
-        # Display the probe names
-        ttk.Label(self.buttonBar, text=curProbe[curProbe.rindex('/')+1:]).grid(row=0, column=1)
-
-        # Add Probe and Label title
-        ttk.Label(self.mainFrame, text="Probe").grid(row=0, column=0)
-        ttk.Label(self.mainFrame, text="Label").grid(row=0, column=1, columnspan=3+self.maxPhotos)
+        # Display the new probe names
+        self.buttonBarDict[(0,1)].configure(text=curProbe[curProbe.rindex('/')+1:])
         
         # Loading Probes
         probeImages = self.getPhotosFromDir(curProbe)
-        self.curProbeImgWidget = [ImageTk.PhotoImage(file=image) for image in probeImages]
-        for i in range(len(self.curProbeImgWidget)):
-            ttk.Label(self.mainFrame, image=self.curProbeImgWidget[i]).grid(row=i+1, column=0)
+        curProbeImgWidget = [ImageTk.PhotoImage(file=image) for image in probeImages]
+        for i in range(len(curProbeImgWidget)):
+            self.probeFrameDict[(i, 0)].configure(image=curProbeImgWidget[i])
+            self.probeFrameDict[(i, 0)].image = curProbeImgWidget[i]
             
         # Loading Rank-5
-        self.curRankPred = [[] for _ in range(self.rank)]
+        curRankPred = [[] for _ in range(self.rank)]
         for i in range(self.rank):
             curGal, curGalScore = self.data[curProbe]['scores'][i][0], self.data[curProbe]['scores'][i][1]
             galImages = self.getPhotosFromDir(curGal)
-            self.curRankPred[i] = [ImageTk.PhotoImage(file=image) for image in galImages]
-            ttk.Label(self.mainFrame, text='Rank {}'.format(i+1)).grid(row=i+1, column=1)
-            ttk.Label(self.mainFrame, text=curGal[curGal.rindex('/')+1:]).grid(row=i+1, column=2)
-            ttk.Label(self.mainFrame, text='Score: {:.3f}'.format(curGalScore)).grid(row=i+1, column=3)
-            for j in range(len(self.curRankPred[i])):
-                ttk.Label(self.mainFrame, image=self.curRankPred[i][j]).grid(row=i+1, column=j+4)
+            curRankPred[i] = [ImageTk.PhotoImage(file=image) for image in galImages]
+            self.galleryFrameDict[(2*i, 2)].configure(text=curGal[curGal.rindex('/')+1:])
+            self.galleryFrameDict[(2*i, 1)].configure(text='Score: {:.3f}'.format(curGalScore))
+            for j in range(len(curRankPred[i])):
+                self.galleryFrameDict[(2*i+1,j+2)].configure(image=curRankPred[i][j])
+                self.galleryFrameDict[(2*i+1,j+2)].image = curRankPred[i][j]
     
     # Iterate through next image
     def next(self):
